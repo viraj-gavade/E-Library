e37129385f2a7175e3e89d8a3e3796459570dd1f
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-13 17:30:59 +0530

    Add user model

---
+++ Models/user.models.js
@@ -0,0 +1,45 @@
+const { match } = require('assert');
+const { kMaxLength } = require('buffer')
+const mongoose = require('mongoose')
+const { type } = require('os')
+
+const usernameRegex = /^(?!.*[-_]{2,})(?![-_])[A-Za-z0-9_-]{3,20}(?<![-_])$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/;
+const emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const UserSchema = new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            kMaxLength:16,
+            required:[true,'Please provide the username'],
+            unique:[true,'Username must be unique'],
+            lowercase:true,
+            match:[usernameRegex,'Please enter the valid username']
+
+        },
+        password:{
+            type:String,
+            kMaxLength:16,
+            required:[true,'Please provide the username'],
+            match:[passwordRegex,'Please enter the valid username']
+
+        },
+        email:{
+            type:String,
+            kMaxLength:16,
+            required:[true,'Please provide the username'],
+            lowercase:true,
+            match:[emailRegex,'Please enter the valid username']
+        },
+        books:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Books"
+            }
+        ]
+    }
+)
+
+
+module.exports = mongoose.model('User',UserSchema)
