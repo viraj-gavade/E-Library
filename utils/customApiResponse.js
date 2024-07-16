04f939745efff5ae71f1a96ed85345a040ce5717
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-16 16:21:41 +0530

    Add custom api response

---
+++ utils/customApiResponse.js
@@ -0,0 +1,10 @@
+class customApiResponse {
+    constructor(statuCode,message,data){
+        this.message = message,
+        this.statuCode=statuCode,
+        this.data = data
+    }
+}
+
+
+module.exports = customApiResponse
