e99de8b4a0291472d5e78873219d82b30eeb35d3
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-16 16:34:57 +0530

    Add Book Router

---
+++ Routes/books.router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const upload = require('../Middlewares/multer.middleware')
+
+
+const BookRouter = express.Router()
+
+
+
+//Main Routes
+
+BookRouter.route('/books').get(GetAllBooks)
+
+BookRouter.route('/books/:bookId').get(GetSingleBook)
+
+BookRouter.route('/books/:bookId').patch(UpdateBook)
+
+BookRouter.route('/books/:bookId').delete(DeleteBook)
+
+BookRouter.route('/books/').post(
+    upload.single('Book'),UploadBook
+) ///Middleware to upload book with the help of cloudinary
+
+
+
+//Exporting the book router
