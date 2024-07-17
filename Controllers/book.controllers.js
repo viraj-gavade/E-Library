01865d1a3522a2f2ef665da379b6382265e9775f
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-17 11:50:03 +0530

    Add get single book controller

--- Controllers/book.controllers.js
+++ Controllers/book.controllers.js
@@ -1,11 +1,33 @@
 const Book = require('../Models/book.models')
 const asyncHandlers = require('../utils/asyncHandler')
+const CustomApiError = require('../utils/customApiError')
+const customApiResponse = require('../utils/customApiResponse')
 
 // Get all books of the library and them by alphabetical order by deafult and some additional sorting fucntionalities
 // Like sorty by year , author in alphabetical odrder
 
 const GetSingleBook =asyncHandlers(  async(req,res)=>{
-    res.send('This is get single book router')
+    const { bookId } = req.params
+    if(!bookId){
+        throw new CustomApiError(
+            401,
+            `There is no such book with Id : ${bookId}`
+        )
+    }
+    const book = await Book.findById(bookId)
+    if(!book){
+         new customApiResponse(
+            200,
+            `There is no such book with Id:${book}`
+         )
+    }
+    return res.status(200).json(
+        new customApiResponse(
+            200,
+            'Book found successfully!',
+            book
+        )
+    )
 })
 const GetAllBooks = asyncHandlers( async (req,res)=>{
     res.send('This is get all book router')
