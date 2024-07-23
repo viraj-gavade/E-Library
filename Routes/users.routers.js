aed620054df039d0f5fff90811e7c2cf5eeb1235
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-23 16:23:55 +0530

    logout route url fixed

--- Routes/users.routers.js
+++ Routes/users.routers.js
@@ -8,7 +8,7 @@ const UserRouter = express.Router()
 UserRouter.route('/register').post(
     upload.single('profileImg'),registerUser)
 UserRouter.route('/login').post(loginUser)
-UserRouter.route('/login').get(logoutUser)
+UserRouter.route('/logout').get(logoutUser)
 UserRouter.route('/change-password').patch(changeUserPassword)
 UserRouter.route('/change-profile-picture').patch(changeUserProfilePicture)
 UserRouter.route('/change-profile-picture').get(refreshAccessTokenandRefreshToken)
