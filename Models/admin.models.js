e8c7d2823099fc00db6749c8d16c0670713f8882
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-25 15:41:58 +0530

    Admin model created

---
+++ Models/admin.models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const usernameRegex = /^(?!.*[-_]{2,})(?![-_])[A-Za-z0-9_-]{3,20}(?<![-_])$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/;
+const emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+const AdminSchema =  mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,'Admin username is compulsory!'],
+        match:[usernameRegex,'Please eneter the username in valid format all should be lowercase'],
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:[true,'Admin password is compulsory!'],
+        match:[passwordRegex,'Please eneter the valid strong password!']
+    },
+    email:{
+        type:String,
+        required:[true,'Admin email in mandatory field!'],
+        match:[emailRegex,'Enter the email in valid format!']
+    },
+    refreshToken:{
+        type:String,
+        required:[False,'Admin refreshtoken is not compulsory!']
+    },
+    profileImg:{
+        type:String,
+        required:[true,'Admin profile picture is neccessary!']
+    }
+})
+
+
+module.exports = mongoose.model('Admin',AdminSchema)
