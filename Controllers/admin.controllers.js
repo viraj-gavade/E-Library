b83bef2462a1135f92c75a14a2364d15dfb77e9a
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-25 15:53:50 +0530

    Basic admin controller setup

--- Controllers/admin.controllers.js
+++ Controllers/admin.controllers.js
@@ -6,7 +6,51 @@ const uploadFile = require('../utils/cloudinary')
 const customApiError = require('../utils/customApiError')
 const customApiResponse = require('../utils/customApiResponse')
 const jwt = require('jsonwebtoken')
+const { changeUserPassword } = require('./user.controllers')
 const options ={
     httpOnly:true,
      secure:true
+}
+
+
+const RegisterAdmin = asyncHandlers(async(req,res)=>{
+    res.send('This is register admin route for test!')
+})
+
+
+
+const LoginAdmin = asyncHandlers(async(req,res)=>{
+    res.send('This is login admin route for test!')
+})
+
+
+
+const LogoutAdmin = asyncHandlers(async(req,res)=>{
+    res.send('This is logout admin route for test!')
+})
+
+
+const ChangeAdminPassword = asyncHandlers(async(req,res)=>{
+    res.send('This is change admin password route for test!')
+})
+
+
+const ChangeAdminUsername = asyncHandlers(async(req,res)=>{
+    res.send('This is change  admin username route for test!')
+})
+
+
+const ChangeAdminEmail = asyncHandlers(async(req,res)=>{
+    res.send('This is change  admin  email route for test!')
+})
+
+
+
+module.exports={
+LoginAdmin,
+LogoutAdmin,
+RegisterAdmin,
+ChangeAdminEmail,
+ChangeAdminUsername,
+changeUserPassword
 }
