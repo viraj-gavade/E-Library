1c2d119103dfe2e85f0887777cbed44959bf10aa
Viraj Gavade <vrajgavade17@gmail.com>
2024-08-03 19:11:02 +0530

    Removed the milter middleware

--- Routes/books.router.js
+++ Routes/books.router.js
@@ -24,18 +24,7 @@ BookRouter.route('/books').get(GetAllBooks)
 
 BookRouter.route('/books/:bookId').get(GetSingleBook)
 
-BookRouter.route('/books/:bookId').patch(
-    upload.fields([
-        {
-            name:'pdfLink',
-            maxCount:1
-        },
-        {
-            name:'CoverImage',
-            maxCount:1
-        }
-    ]),
-    UpdateBook)
+BookRouter.route('/books/:bookId').patch(UpdateBook)
 
 BookRouter.route('/books/:bookId').delete(DeleteBook)
 
