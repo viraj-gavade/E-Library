4e680793a3ae7c31f2f77503b6af392ff7d02c44
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-16 11:40:47 +0530

    "Added JSDoc comments and reformatted code in book.models.js and user.models.js files, including schema definitions, instance methods, and middleware functions."

--- Models/book.models.js
+++ Models/book.models.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
 
+/**
+ * Mongoose schema for Book collection
+ * 
+ * @schema Book
+ * @description Defines the structure and validation rules for book documents
+ * 
+ * Fields:
+ * @property {String} author - Name of the book's author
+ * @property {String} CoverImage - URL/path to the book's cover image
+ * @property {String} title - Title of the book
+ * @property {Number} publishedInYear - Year book was published (4 digits max)
+ * @property {String} pdfLink - URL/path to the book's PDF file
+ * @property {Number} copies - Number of available copies (defaults to 10)
+ * @property {Boolean} available - Book availability status (defaults to true)
+ * @property {String} category - Book category/genre (defaults to 'Book')
+ * @property {ObjectId} uploadedBy - Reference to User who uploaded the book
+ * 
+ * Includes timestamps:
+ * - createdAt: Date document was created
+ * - updatedAt: Date document was last modified
+ */
 const BookSchema = new mongoose.Schema({
   author: {
-    type:String,
+    type: String,
     required: true
   },
   CoverImage: {
@@ -15,35 +36,44 @@ const BookSchema = new mongoose.Schema({
   },
   publishedInYear: {
     type: Number,
-    maxLength: 4
+    maxLength: 4    // Limits year to 4 digits (e.g., 2024)
   },
   pdfLink: {
     type: String,
-    type: String,
+    type: String,    // Note: Duplicate type declaration
     required: [true, 'CoverImage must be provided!']
   },
   copies: {
     type: Number,
-    default: 10
+    default: 10     // Default number of copies
   },
-  available:{
-    type:Boolean,
-    default:true
-    
+  available: {
+    type: Boolean,
+    default: true   // Book is available by default
   },
-  category:{
-    type:String,
-    required:[true,'Please provide the category'],
-    default:'Book'
+  category: {
+    type: String,
+    required: [true, 'Please provide the category'],
+    default: 'Book'
   },
-  uploadedBy:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"     // References User model
   }
-}, { timestamps: true });
+}, { timestamps: true });  // Adds createdAt and updatedAt fields
 
+/**
+ * Instance method to check book availability
+ * 
+ * @method isAvailable
+ * @returns {Boolean} True if any copy is available, false otherwise
+ * 
+ * Note: Current implementation may need revision as it assumes 'copies'
+ * is an array with 'available' property, but schema shows 'copies' as Number
+ */
 BookSchema.methods.isAvailable = function () {
     return this.copies.some(copy => copy.available);
-  };
+};
 
+// Create and export Book model
 module.exports = mongoose.model('Book', BookSchema);
--- Models/user.models.js
+++ Models/user.models.js
@@ -3,83 +3,128 @@ const { kMaxLength } = require('buffer')
 const mongoose = require('mongoose')
 const { type } = require('os')
 const bcryptjs = require('bcryptjs')
-
 const jwt = require('jsonwebtoken')
 
+/**
+ * Mongoose schema for User collection
+ * 
+ * @schema User
+ * @description Defines the structure for user documents with authentication capabilities
+ * 
+ * Main features:
+ * - Username/password authentication
+ * - Password hashing with bcrypt
+ * - JWT token generation
+ * - Profile management
+ * - Book collection tracking
+ */
 const UserSchema = new mongoose.Schema(
     {
-        username:{
-            type:String,
-            kMaxLength:16,
-            required:[true,'Please provide the username'],
-            unique:[true,'Username must be unique'],
-            lowercase:true,
-
+        username: {
+            type: String,
+            kMaxLength: 16,                           // Maximum length for username
+            required: [true, 'Please provide the username'],
+            unique: [true, 'Username must be unique'],
+            lowercase: true,                          // Store username in lowercase
         },
-        password:{
-            type:String,
-            // kMaxLength:16,
-            required:[true,'Please provide the username']
-
+        password: {
+            type: String,
+            // kMaxLength: 16,                        // Commented out as it might limit hashed password
+            required: [true, 'Please provide the username']  // Note: Error message should be updated
         },
-        email:{
-            type:String,
-            kMaxLength:16,
-            required:[true,'Please provide the username'],
-            lowercase:true,
-            unique:true
+        email: {
+            type: String,
+            kMaxLength: 16,                           // Maximum length for email
+            required: [true, 'Please provide the username'],  // Note: Error message should be updated
+            lowercase: true,
+            unique: true
         },
-        profileImg:{
-            type:String,
-            required:false
+        profileImg: {
+            type: String,
+            required: false                           // Optional profile image
         },
-        bio:{
-            type:String,
-            required:false,
-            default:''
+        bio: {
+            type: String,
+            required: false,
+            default: ''                               // Empty bio by default
         },
-        refreshToken:{
-            type:String,
+        refreshToken: {
+            type: String,                             // Stores JWT refresh token
         },
-        books:[
+        books: [
             {
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"Books"
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Books"                          // References to user's book collection
             }
         ],
-       
     },
-    {timestamps:true}
+    { timestamps: true }                             // Adds createdAt and updatedAt fields
 )
 
-UserSchema.pre('save',async function (next){
-    if(!this.isModified('password')){
+/**
+ * Pre-save middleware to hash password
+ * 
+ * Triggers before saving user document:
+ * 1. Checks if password was modified
+ * 2. Generates salt with cost factor 10
+ * 3. Hashes password with salt
+ * 
+ * @middleware
+ */
+UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
         return next()
     }
     const salt = await bcryptjs.genSalt(10)
-    this.password = await bcryptjs.hash(this.password,salt)
+    this.password = await bcryptjs.hash(this.password, salt)
     next()
 })
 
-
-UserSchema.methods.isPasswordCorrect = async function(UserPassword){
-    return await bcryptjs.compare(UserPassword,this.password)
-
+/**
+ * Instance method to verify password
+ * 
+ * @method isPasswordCorrect
+ * @param {string} UserPassword - Plain text password to verify
+ * @returns {Promise<boolean>} True if password matches, false otherwise
+ */
+UserSchema.methods.isPasswordCorrect = async function(UserPassword) {
+    return await bcryptjs.compare(UserPassword, this.password)
 }
+
+/**
+ * Instance method to generate JWT access token
+ * 
+ * @method generateAccessToken
+ * @returns {Promise<string>} JWT access token
+ * 
+ * Token contains:
+ * - User ID
+ * - Expiration as per ACCESS_TOKEN_EXPIRY env variable
+ * - Signed with ACCESS_TOKEN_SECRETE
+ */
 UserSchema.methods.generateAccessToken = async function() {
     const accessToken = await jwt.sign({
-        _id:this._id
-    },process.env.ACCESS_TOKEN_SECRETE,{expiresIn:process.env.ACCESS_TOKEN_EXPIRY})
+        _id: this._id
+    }, process.env.ACCESS_TOKEN_SECRETE, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
     return accessToken
 }
 
-
+/**
+ * Instance method to generate JWT refresh token
+ * 
+ * @method generateRefreshToken
+ * @returns {Promise<string>} JWT refresh token
+ * 
+ * Token contains:
+ * - User ID
+ * - Expiration as per REFRESH_TOKEN_EXPIRY env variable
+ * - Signed with REFRESH_TOKEN_SECRETE
+ */
 UserSchema.methods.generateRefreshToken = async function() {
     const accessToken = await jwt.sign({
-        _id:this._id
-    },process.env.REFRESH_TOKEN_SECRETE,{expiresIn:process.env.REFRESH_TOKEN_EXPIRY})
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRETE, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
     return accessToken
-},
+}
 
-
-module.exports = mongoose.model('User',UserSchema)
+module.exports = mongoose.model('User', UserSchema)
