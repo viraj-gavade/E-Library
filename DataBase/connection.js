677d7d801b9a5f333b4507a3d5e7f84a51787e93
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-16 11:35:25 +0530

    "Added JSDoc comments to connectDB function, removed unnecessary whitespace, and commented out deprecated Mongoose options."

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose')
 
-
+/**
+ * Establishes connection to MongoDB database
+ * 
+ * @param {string} url - MongoDB connection string URL
+ * @returns {Promise<mongoose.Connection>} Mongoose connection object
+ * 
+ * Configuration options:
+ * - useNewUrlParser: Enables new URL string parser
+ * - useUnifiedTopology: Enables new Server Discovery and Monitoring engine
+ * 
+ * Note: The following options are deprecated in newer Mongoose versions:
+ * - useCreateIndex
+ * - useFindAndModify
+ * 
+ * @throws {Error} Exits process with code 1 if connection fails
+ */
 const connectDB =  (url)=>{
     try {
       return mongoose.connect(url,{
         useNewUrlParser: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
+        // useCreateIndex: true,        // Deprecated in Mongoose 6
+        // useFindAndModify: false,     // Deprecated in Mongoose 6
         useUnifiedTopology: true,
       }
        )
@@ -16,7 +31,5 @@ const connectDB =  (url)=>{
         process.exit(1)
     }
 }
-
-
 
 module.exports = connectDB
